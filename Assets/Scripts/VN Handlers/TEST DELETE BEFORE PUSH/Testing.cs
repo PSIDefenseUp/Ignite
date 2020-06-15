221e78e9d0f48bb3f07cb98e62490bb6156ce797
Paul Lederer <plederer93@gmail.com>
2020-06-15 02:47:46 -0500

    Made test environment for VN Segments, Broke apart spritesheets for animation mapping

---
+++ Assets/Scripts/VN Handlers/TEST DELETE BEFORE PUSH/Testing.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    DialogueSystem dialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = DialogueSystem.instance;
+    }
+
+
+    // Update is called once per frame
+    public string[] s = new string[]
+    {
+        "WAIII ACAB AKARI ACAB DAISUKE:Akari",
+        "ACAB WAIIII",
+        "Google 40% Cops for a cool suprise",
+    };
+    int index = 0;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!dialogue.isSpeaking || dialogue.isWaitingForUserInput)
+            {
+                Say(s[index]);
+                index++;
+            }
+        }
+    }
+    void Say(string s)
+    {
+        string[] parts = s.Split(':');
+        string speech = parts[0];
+        string speaker = (parts.Length >= 2) ? parts[1]: "";
+        dialogue.Say(speech, speaker);
+
+    }
+}
