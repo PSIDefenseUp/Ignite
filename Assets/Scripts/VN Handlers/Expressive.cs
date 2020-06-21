0b2ff92f0f40037b211a8f583b709b591a74d0c4
Paul Lederer <plederer93@gmail.com>
2020-06-21 05:20:44 -0500

    Created Expressive component

---
+++ Assets/Scripts/VN Handlers/Expressive.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Expressive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Dictionary<string,Sprite> Expressions;
+    private Image myImage;
+    void Start()
+    {
+        myImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeExpression(string Name)
+    {
+        myImage.sprite = Expressions[Name];
+    }
+}
